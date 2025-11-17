20f5f8033adb06eff53638e94e1ba015b2965a9d
EdgarBL1 <bllbedgarr@gmail.com>
2025-11-16 20:29:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     keywords='python v2ray multi-v2ray vmess socks5 vless trojan xray xtls reality',
     author='Jrohy',
     author_email='euvkzx@gmail.com',
-    url='https://github.com/Jrohy/multi-v2ray',
+    url='https://github.com/EdgarBL1/blgen',
     license='GPL',
     packages=find_packages(),
     include_package_data=True,
